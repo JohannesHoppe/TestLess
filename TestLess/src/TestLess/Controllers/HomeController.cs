b5392c2bbccb1424a859c96e348ae752707d2485
gusti <augustin.calin@gmail.com>
2016-04-03 17:52:47 +0200

    2nd

---
+++ TestLess/src/TestLess/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Hosting;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestLess.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _env;
+
+        public HomeController(IHostingEnvironment env) {
+            _env = env;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ TestLess/src/TestLess/gulpfile.js
@@ -0,0 +1,33 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    less = require("gulp-less"),
+    lessPluginCleanCss = require('less-plugin-clean-css'),
+    cleanCSSPlugin = new lessPluginCleanCss({ advanced: true }),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require("gulp-rename");
+
+var paths = {
+    lessConfig: 'styles/main.config.less',
+    webroot: "./wwwroot/"
+};
+
+gulp.task("less-min", function () {
+    return gulp.src(paths.lessConfig)
+        .pipe(sourcemaps.init())
+        .pipe(less({ plugins: [cleanCSSPlugin] }))
+        .pipe(rename('app-bundle.min.css'))
+        .pipe(sourcemaps.write('/.'))
+        .pipe(gulp.dest(paths.webroot + '/css'));
+});
+gulp.task("less", function () {
+    return gulp.src(paths.lessConfig)
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(rename('app-bundle.css'))
+        .pipe(sourcemaps.write('/.'))
+        .pipe(gulp.dest(paths.webroot + '/css'));
+});
